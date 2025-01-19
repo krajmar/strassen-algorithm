5cab234ae5b4c60329c7c71ae94e5cd326a9cfb4
krajmar <krajoskimario@gmail.com>
2025-01-19 21:05:36 +0100

    Implementirano da mozhe da raboti i so matrici so neparni dimenzii

--- Main.java
+++ Main.java
@@ -7,37 +7,38 @@ public class Main {
         //spored zadacata, korisnikot ja zadava goleminata na matricite
         System.out.println("Specify sizes of matrices: ");
         Scanner sc = new Scanner(System.in);
-        //ArrayList<Double>runtimesSeq=new ArrayList<Double>();
         double startTime = System.currentTimeMillis();
 
         int sizeMat = sc.nextInt();
+
         Sequential seq = new Sequential(sizeMat);
 
+
         //ja deli prvata matrica na cetvrtini (4 pomali kvadratni matrici)
-        ArrayList<int[][]>arrList1=seq.splitMatrix(seq.getMatrix1());
+        ArrayList<int[][]> arrList1 = seq.splitMatrix(seq.getMatrix1());
         //ja deli vtorata matrica na cetvrtini (4 pomali kvadratni matrici)
-        ArrayList<int[][]>arrList2=seq.splitMatrix(seq.getMatrix2());
+        ArrayList<int[][]> arrList2 = seq.splitMatrix(seq.getMatrix2());
 
         //ovde ke se zacuvaat finalnite cetvrtini (4 pomali kvadratni matrici)
         //potoa samo se spojuvaat vo krajnata matrica
-        ArrayList<int[][]>finalArrayList=new ArrayList<>();
+        ArrayList<int[][]> finalArrayList = new ArrayList<>();
 
         //strassen algorithm matrici spored wikipedia
-        int [][] M1 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(0),arrList1.get(3)),
-                seq.sumMatrix(arrList2.get(0),arrList2.get(3)));
-        int [][] M2 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(1),arrList1.get(3)),
+        int[][] M1 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(0), arrList1.get(3)),
+                seq.sumMatrix(arrList2.get(0), arrList2.get(3)));
+        int[][] M2 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(1), arrList1.get(3)),
                 arrList2.get(0));
-        int [][] M3 = seq.multiplyMatrix(arrList1.get(0),seq.subtractMatrix(arrList2.get(2),arrList2.get(3)));
-        int [][] M4 = seq.multiplyMatrix(arrList1.get(3),seq.subtractMatrix(arrList2.get(1),arrList2.get(0)));
-        int [][] M5 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(0),arrList1.get(2)),arrList2.get(3));
-        int [][] M6 = seq.multiplyMatrix(seq.subtractMatrix(arrList1.get(1),arrList1.get(0)),seq.sumMatrix(arrList2.get(0),arrList2.get(2)));
-        int [][] M7 = seq.multiplyMatrix(seq.subtractMatrix(arrList1.get(2),arrList1.get(3)),seq.sumMatrix(arrList2.get(1),arrList2.get(3)));
+        int[][] M3 = seq.multiplyMatrix(arrList1.get(0), seq.subtractMatrix(arrList2.get(2), arrList2.get(3)));
+        int[][] M4 = seq.multiplyMatrix(arrList1.get(3), seq.subtractMatrix(arrList2.get(1), arrList2.get(0)));
+        int[][] M5 = seq.multiplyMatrix(seq.sumMatrix(arrList1.get(0), arrList1.get(2)), arrList2.get(3));
+        int[][] M6 = seq.multiplyMatrix(seq.subtractMatrix(arrList1.get(1), arrList1.get(0)), seq.sumMatrix(arrList2.get(0), arrList2.get(2)));
+        int[][] M7 = seq.multiplyMatrix(seq.subtractMatrix(arrList1.get(2), arrList1.get(3)), seq.sumMatrix(arrList2.get(1), arrList2.get(3)));
 
 
-        int [][] matI=seq.sumMatrix(seq.subtractMatrix(seq.sumMatrix(M1,M4),M5),M7); //topleft za final matrix
-        int [][] matJ=seq.sumMatrix(M2,M4); //bottomleft za final matrix
-        int [][] matK=seq.sumMatrix(M3,M5); //topright za final matrix
-        int [][] matL=seq.sumMatrix(seq.sumMatrix(seq.subtractMatrix(M1,M2),M3),M6); //bottomright za final matrix
+        int[][] matI = seq.sumMatrix(seq.subtractMatrix(seq.sumMatrix(M1, M4), M5), M7); //topleft za final matrix
+        int[][] matJ = seq.sumMatrix(M2, M4); //bottomleft za final matrix
+        int[][] matK = seq.sumMatrix(M3, M5); //topright za final matrix
+        int[][] matL = seq.sumMatrix(seq.sumMatrix(seq.subtractMatrix(M1, M2), M3), M6); //bottomright za final matrix
 
         finalArrayList.add(matI);
         finalArrayList.add(matJ);
@@ -48,8 +49,8 @@ public class Main {
 
         double endTime = System.currentTimeMillis();
         double duration = endTime - startTime; //presmetaj kolku vreme se executnuva algoritmot
-        if(duration<=600000){ //ako e povekje od 10min ne pishuvaj
-            String fileName = "C:\\Users\\User3245\\Desktop\\Strassen Algorithm\\program_runtime.csv";
+        if (duration <= 600000) { //ako e povekje od 10min ne pishuvaj
+            String fileName = "program_runtime.csv";
             try (FileWriter writer = new FileWriter(fileName, true)) {
                 String csvData = duration + " ms";
 
@@ -62,14 +63,6 @@ public class Main {
             } catch (IOException e) {
                 System.err.println("Error writing to CSV file: " + e.getMessage());
             }
-        }
-        else System.out.println("More than 10mins, stop the testing.");
+        } else System.out.println("More than 10mins, stop the testing.");
     }
-    /*public static void printMatrix(int[][] mat) {
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                System.out.print(mat[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }*/ }
+}
--- Sequential.java
+++ Sequential.java
@@ -13,125 +13,171 @@ public class Sequential {
         return matrix2;
     }
 
-    public Sequential(){}
+    public Sequential() {
+    }
 
-    public Sequential(int size){
-        this.matrix1=new int[size][size];
-        this.matrix2=new int[size][size];
-        this.finalmatrix=new int[size][size];
+    public Sequential(int size) {
+        this.matrix1 = new int[size][size];
+        this.matrix2 = new int[size][size];
+        this.finalmatrix = new int[size][size];
 
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                int r1=(int) (Math.random()*10);
-                int r2=(int) (Math.random()*10);
-                this.matrix1[i][j]=r1;
-                this.matrix2[i][j]=r2;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int r1 = (int) (Math.random() * 10);
+                int r2 = (int) (Math.random() * 10);
+                this.matrix1[i][j] = r1;
+                this.matrix2[i][j] = r2;
             }
         }
     }
-    /*public Sequential(int [][] matrix1, int [][] matrix2) {
-        this.matrix1 = matrix1;
-        this.matrix2 = matrix2;
-    }*/
 
+    //funkcija za zgolemuvanje na dimenzijata na matricata za 1
+    public int[][] padMatrix(int[][] matrix) {
+        int originalSize = matrix.length;
+        int newSize = (originalSize % 2 == 0) ? originalSize : originalSize + 1;
 
-
-    public ArrayList<int[][]> splitMatrix(int[][] matrix){
-        int[][] submat1=new int[matrix.length/2][matrix.length/2];
-        int[][] submat2=new int[matrix.length/2][matrix.length/2];
-        int [][] submat3=new int[matrix.length/2][matrix.length/2];
-        int [][] submat4=new int[matrix.length/2][matrix.length/2];
-        //topleft
-        for(int i=0;i<matrix.length/2;i++){
-            System.arraycopy(matrix[i], 0, submat1[i], 0, matrix.length / 2);
-        }
-        //bottomleft
-        for(int i=matrix.length/2;i<matrix.length;i++){
-            System.arraycopy(matrix[i], 0, submat2[i - matrix.length / 2], 0, matrix.length / 2);
-        }
-        //topright
-        for(int i=0;i<matrix.length/2;i++){
-            if (matrix.length - matrix.length / 2 >= 0)
-                System.arraycopy(matrix[i], matrix.length / 2, submat3[i], 0, matrix.length - matrix.length / 2);
-        }
-        //bottomright
-        for(int i=matrix.length/2;i<matrix.length;i++){
-            if (matrix.length - matrix.length / 2 >= 0)
-                System.arraycopy(matrix[i], matrix.length / 2, submat4[i - matrix.length / 2], 0, matrix.length - matrix.length / 2);
-
+        if (newSize == originalSize) {
+            return matrix; // No padding needed
         }
 
+        int[][] paddedMatrix = new int[newSize][newSize];
+        for (int i = 0; i < originalSize; i++) {
+            System.arraycopy(matrix[i], 0, paddedMatrix[i], 0, originalSize);
+        }
 
-        ArrayList<int[][]>arrList=new ArrayList<>();
+        return paddedMatrix;
+    }
+    //funkcija za namaluvanje na dimenzijata na finalnata matrica za 1
+    public int[][] unpadMatrix(int[][] matrix, int originalSize) {
+        int[][] unpaddedMatrix = new int[originalSize][originalSize];
+        for (int i = 0; i < originalSize; i++) {
+            System.arraycopy(matrix[i], 0, unpaddedMatrix[i], 0, originalSize);
+        }
+        return unpaddedMatrix;
+    }
+
+    public ArrayList<int[][]> splitMatrix(int[][] matrix) {
+        ArrayList<int[][]> arrList = new ArrayList<>();
+        int size = matrix.length;
+        int[][] pmatrix = matrix; // Default so orginalnata matrica
+
+        // ako e neparna, zgolemi ja dolzinata i sirinata za 1 kolona i red
+        if (size % 2 == 1) {
+            pmatrix = padMatrix(matrix);
+            size += 1; // se zgolemuva goleminata
+        }
+
+        // gi kreirame submatricite
+        int[][] submat1 = new int[size / 2][size / 2];
+        int[][] submat2 = new int[size / 2][size / 2];
+        int[][] submat3 = new int[size / 2][size / 2];
+        int[][] submat4 = new int[size / 2][size / 2];
+
+        //top-left
+        for (int i = 0; i < size / 2; i++) {
+            System.arraycopy(pmatrix[i], 0, submat1[i], 0, size / 2);
+        }
+
+        //bottom-left
+        for (int i = size / 2; i < size; i++) {
+            System.arraycopy(pmatrix[i], 0, submat2[i - size / 2], 0, size / 2);
+        }
+
+        //top-right
+        for (int i = 0; i < size / 2; i++) {
+            System.arraycopy(pmatrix[i], size / 2, submat3[i], 0, size / 2);
+        }
+
+        //bottom-right
+        for (int i = size / 2; i < size; i++) {
+            System.arraycopy(pmatrix[i], size / 2, submat4[i - size / 2], 0, size / 2);
+        }
+
         arrList.add(submat1);
         arrList.add(submat2);
         arrList.add(submat3);
         arrList.add(submat4);
+
         return arrList;
     }
-    public void mergeMatrix(ArrayList<int[][]>arrList){
-        //topleft
-        for(int i=0;i<arrList.get(0).length;i++){
-            System.arraycopy(arrList.get(0)[i], 0, this.finalmatrix[i], 0, arrList.get(0).length);
-        }
-        //bottomleft
-        for(int i=0;i<arrList.get(0).length;i++){
-            System.arraycopy(arrList.get(1)[i], 0, this.finalmatrix[arrList.get(1).length + i], 0, arrList.get(0).length);
-        }
-        //topright
-        for(int i=0;i<arrList.get(0).length;i++){
-            System.arraycopy(arrList.get(2)[i], 0, this.finalmatrix[i], arrList.get(2).length, arrList.get(0).length);
-        }
-        //bottomright
-        for(int i=0;i<arrList.get(0).length;i++){
-            System.arraycopy(arrList.get(3)[i], 0, this.finalmatrix[arrList.get(3).length + i], arrList.get(3).length, arrList.get(0).length);
+
+
+    public void mergeMatrix(ArrayList<int[][]> arrList) {
+        int size = arrList.get(0).length; //site se od ista golemina
+        int finalSize = size * 2; // spojuvame 4 submatrici taka da dolzinata na edna *2 = dolzina na celata
+
+        if (this.finalmatrix.length < finalSize) {
+            this.finalmatrix = new int[finalSize][finalSize];
         }
 
-        /*
+        //top-left
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(arrList.get(0)[i], 0, this.finalmatrix[i], 0, size);
+        }
+
+        //bottom-left
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(arrList.get(1)[i], 0, this.finalmatrix[size + i], 0, size);
+        }
+
+        //top-right
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(arrList.get(2)[i], 0, this.finalmatrix[i], size, size);
+        }
+
+        //bottom-right
+        for (int i = 0; i < size; i++) {
+            System.arraycopy(arrList.get(3)[i], 0, this.finalmatrix[size + i], size, size);
+        }
+
+        //ako sme dodale edna kolona i red na pocetnata, sega gi odzemame od finalnata
+        if (this.matrix1.length % 2 != 0) {
+            this.finalmatrix = unpadMatrix(this.finalmatrix, this.matrix1.length);
+        }
+
         printMatrix(this.matrix1);
         System.out.println();
         printMatrix(this.matrix2);
         System.out.println();
         printMatrix(this.finalmatrix);
-         */
     }
 
-    public int[][] sumMatrix(int [][] matrix1, int [][] matrix2){
-        int [][] newMat = new int [matrix1.length][matrix1.length];
+
+    public int[][] sumMatrix(int[][] matrix1, int[][] matrix2) {
+        int[][] newMat = new int[matrix1.length][matrix1.length];
         //spored zadacata ako se dvete matrici so ista dolzina i sirina
-        for(int i=0;i<matrix1.length;i++){
-            for(int j=0;j<matrix1.length;j++){
-                newMat[i][j]+=matrix1[i][j];
-                newMat[i][j]+=matrix2[i][j];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1.length; j++) {
+                newMat[i][j] += matrix1[i][j];
+                newMat[i][j] += matrix2[i][j];
             }
         }
         return newMat;
     }
 
-    public int[][] subtractMatrix(int [][] matrix1, int [][] matrix2){
-        int [][] newMat = new int [matrix1.length][matrix1.length];
+    public int[][] subtractMatrix(int[][] matrix1, int[][] matrix2) {
+        int[][] newMat = new int[matrix1.length][matrix1.length];
         //spored zadacata ako se dvete matrici so ista dolzina i sirina
-        for(int i=0;i<matrix1.length;i++){
-            for(int j=0;j<matrix1.length;j++){
-                newMat[i][j]+=matrix1[i][j];
-                newMat[i][j]-=matrix2[i][j];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1.length; j++) {
+                newMat[i][j] += matrix1[i][j];
+                newMat[i][j] -= matrix2[i][j];
             }
         }
         return newMat;
     }
 
-    public int[][] multiplyMatrix(int [][] A, int [][] B) {
-        int row1=A.length;
-        int col1=A[0].length;
-        int row2=B.length;
-        int col2=B[0].length;
+    public int[][] multiplyMatrix(int[][] A, int[][] B) {
+        int row1 = A.length;
+        int col1 = A[0].length;
+        int row2 = B.length;
+        int col2 = B[0].length;
 
         if (row2 != col1) {
             return null;
         }
         int[][] C = new int[row1][col2];
 
-        // Multiply the two matrices
         for (int i = 0; i < row1; i++) {
             for (int j = 0; j < col2; j++) {
                 for (int k = 0; k < row2; k++)
@@ -141,5 +187,13 @@ public class Sequential {
         return C;
     }
 
+    public void printMatrix(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
 
+    }
 }
